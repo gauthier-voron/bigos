47db2305a83e9e4e60f2ff0ada1a8b4a0d0a1f94
Gauthier Voron <gauthier.voron@lip6.fr>
2014-05-23 14:28:45 +0200

    Fix some bugs   - in the Makefile, avoid using /bin/sh with no "source" command   - in bench-parsec, runs the true benchmark

--- benchmark/kvm-compare-host-guest/bench-parsec-3.0.sh
+++ benchmark/kvm-compare-host-guest/bench-parsec-3.0.sh
@@ -12,11 +12,12 @@ rm -rf log/* || exit 1
 
 
 for i in `seq 1 3` ; do
-  parsecmgmt -a run -p streamcluster -i test -n 2 || exit 1
+  parsecmgmt -a run -p streamcluster -i native -n 48 || exit 1
 done
 [ $1 ] && mv log/*/*.log "../$1-NI/"
 
 for i in `seq 1 3` ; do
-  numactl --interleave=all parsecmgmt -a run -p streamcluster -i test -n 2 || exit 1
+  numactl --interleave=all parsecmgmt -a run -p streamcluster -i native -n 48 || exit 1
 done
 [ $1 ] && mv log/*/*.log "../$1-I/"
+
