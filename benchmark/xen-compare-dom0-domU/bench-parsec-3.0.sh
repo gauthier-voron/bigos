16577958647cc65821194bdcfb9c05d33a1d7298
Gauthier Voron <gauthier.voron@lip6.fr>
2014-05-26 17:56:11 +0200

    Add benchmark with 2 DomU each running with 24 vCPUs but executing a 48 tasks benchmark   - one with automatic placement   - the other pith pinned CPUs

--- benchmark/xen-compare-dom0-domU/bench-parsec-3.0.sh
+++ benchmark/xen-compare-dom0-domU/bench-parsec-3.0.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 if [ $1 ] ; then
-  mkdir -p $1-NI || exit 1
-  mkdir -p $1-I || exit 1
+  mkdir -p $1 || exit 1
 fi
 
 cd parsec-3.0 || exit 1
@@ -14,10 +13,4 @@ rm -rf log/* || exit 1
 for i in `seq 1 3` ; do
   parsecmgmt -a run -p streamcluster -i native -n 48 || exit 1
 done
-[ $1 ] && mv log/*/*.log "../$1-NI/"
-
-for i in `seq 1 3` ; do
-  numactl --interleave=all parsecmgmt -a run -p streamcluster -i native -n 48 || exit 1
-done
-[ $1 ] && mv log/*/*.log "../$1-I/"
-
+[ $1 ] && mv log/*/*.log "../$1/"
