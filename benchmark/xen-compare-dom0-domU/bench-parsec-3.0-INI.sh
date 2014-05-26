d7e730a00bf5876e4d4c8b742e35be1c651ef40a
Gauthier Voron <gauthier.voron@lip6.fr>
2014-05-26 18:01:56 +0200

    Add the data to plot conversion script with some logs renaming

---
+++ benchmark/xen-compare-dom0-domU/bench-parsec-3.0-INI.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+if [ $1 ] ; then
+  mkdir -p $1-NI || exit 1
+  mkdir -p $1-I || exit 1
+fi
+
+cd parsec-3.0 || exit 1
+source ./env.sh
+
+rm -rf log/* || exit 1
+
+
+for i in `seq 1 3` ; do
+  parsecmgmt -a run -p streamcluster -i native -n 48 || exit 1
+done
+[ $1 ] && mv log/*/*.log "../$1-NI/"
+
+for i in `seq 1 3` ; do
+  numactl --interleave=all parsecmgmt -a run -p streamcluster -i native -n 48 || exit 1
+done
+[ $1 ] && mv log/*/*.log "../$1-I/"
+
