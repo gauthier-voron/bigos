5cda3d580a756ae0da76be2bd58506f8ebe1ac4a
Gauthier Voron <gauthier.voron@lip6.fr>
2014-06-25 15:12:50 +0200

    feat(gnuplot): add a filled curve option
    Add a user option to display down-filled curves instead of lines.

--- xenperf/tool/gnuplot.pl
+++ xenperf/tool/gnuplot.pl
@@ -22,6 +22,7 @@ sub _die
 my $line;
 my $base;
 my $delta;
+my $style;
 my $index;
 my @fields;
 
@@ -33,6 +34,7 @@ my $x11;
 my $verbose;
 my $title = '';
 my $logscale;
+my $filledcurve;
 my %bases;
 my %events;
 
@@ -41,6 +43,7 @@ Getopt::Long::Configure("bundling");
 GetOptions (
     'verbose|v' => \$verbose,
     'logscale|l' => \$logscale,
+    'filledcurve|f' => \$filledcurve,
     'x11|x' => \$x11,
     'color=s' => \%bases,
     'event=s' => \%events,
@@ -93,7 +96,13 @@ foreach (@fields) {
     $base += $delta;
     $base = sprintf '%x', $base;
 
-    print $gnuplotfd "'$data' using $index with lines title '"
+    if ($filledcurve) {
+	$style = 'filledcurve x1';
+    } else {
+	$style = 'lines';
+    }
+
+    print $gnuplotfd "'$data' using $index with $style title '"
 	. ($events{"$1:$2"} || "0x$1:$2") . " (core $3)' "
 	. "lc rgb '#$base', \\";
     $index++;
